a0dbdcfd705b5a73140efbcd31d60f8f702acee7
carlos rodriguez <car_1998_@hotmail.es>
2019-05-19 19:59:22 +0200

    try install storybook and create card component

---
+++ src/componentes/Carta.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Carta.css'
+export default function Carta({ carta }) {
+  return (
+    <div className="carta">
+      {carta.numero}
+      {carta.palo}
+    </div>
+  )
+}
